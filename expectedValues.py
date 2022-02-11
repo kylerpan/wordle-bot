96bfaa3fac94e0dc71ac5299e1c8b796fe793c2c
Kyle Pan <kylerpan691@gmail.com>
2022-02-10 17:40:13 -0800

    made user interface better

--- expectedValues.py
+++ expectedValues.py
@@ -3,7 +3,35 @@ from colors import colorCombinations
 from copy import copy
 from math import log2
 
-def allExpectedValues(wordInput):
+def progressBar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iterable    - Required  : iterable object (Iterable)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    total = len(iterable)
+    # Progress Bar Printing Function
+    def printProgressBar (iteration):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Initial Call
+    printProgressBar(0)
+    # Update Progress Bar
+    for i, item in enumerate(iterable):
+        yield item
+        printProgressBar(i + 1)
+    # Print New Line on Complete
+    print()
+
+def initialExpectedValues(wordInput):
     allExpected = []
 
     for combination in colorCombinations:
@@ -45,7 +73,7 @@ def allExpectedValues(wordInput, wordsLeft):
         infoBit = log2(1/probability) if probability != 0 else 0
         allExpected.append(probability * infoBit)
     
-    print(wordInput, sum(allExpected))
+    # print(wordInput, sum(allExpected))
     return sum(allExpected)
 
 def changeValidWords(wordInput, colorResult, wordsLeft):
@@ -58,14 +86,21 @@ def changeValidWords(wordInput, colorResult, wordsLeft):
         else:
             result = [word for word in result if word[i] == wordInput[i]]
 
-    wordsLeftExpected = sorted([(word, allExpectedValues(word, result)) for word in possibleWords], key=lambda x: x[1], reverse=True)
-    print(result)
-    print(len(result))
-    return wordsLeftExpected, result
+    # wordsLeftExpected = sorted([(word, allExpectedValues(word, result)) for word in possibleWords], key=lambda x: x[1], reverse=True)
+    wordsLeftExpected = []
+    for word in progressBar(possibleWords, prefix='Progress', suffix='Complete', length=50):
+        wordsLeftExpected.append((word, allExpectedValues(word, result)))
+    wordsLeftExpected = sorted(wordsLeftExpected, key=lambda x: x[1], reverse=True)
+    
+    print(f'\n{len(result)} Words Left: ')
+    for i in range(len(result)):
+        print(result[i], end=" ")
+        if i % 15 == 0 and i != 0: print()
+    return wordsLeftExpected, result, result
 
 
 if __name__ == '__main__':
-    allWordsExpected = sorted([(word, allExpectedValues(word)) for word in possibleWords], key=lambda x: x[1], reverse=True)
+    allWordsExpected = sorted([(word, initialExpectedValues(word)) for word in possibleWords], key=lambda x: x[1], reverse=True)
     f = open("allWordsExpected.txt", "w")
     f.write('(')
     for word in allWordsExpected:
--- wordle.py
+++ wordle.py
@@ -1,4 +1,5 @@
 from words import possibleWords
+from colors import colors
 from initialExpectValue import initialExpected
 from expectedValues import changeValidWords
 
@@ -9,14 +10,23 @@ if __name__ == '__main__':
     for i in range(15):
         print(initialExpected[i][0], initialExpected[i][1])
     while True:
-        print(wordsLeft)
-        word = input('Please input the first word you chose (e.g. crane): ')
-        result = input('Please input the color results (e.g. bbygb): ')
-        if (word == 'quit' or result == 'quit'): break
+        word = input('\nPlease input the WORD you chose (e.g. crane): ')
+        if (word not in possibleWords):
+            print('Please enter a valid 5 letter WORD\n')
+            continue
+
+        colorResult = input('Please input the COLOR RESULTS (e.g. bbygb): ')
+        if (len(colorResult) != 5 or all([True if character not in colors else False for character in colorResult])):
+            print('Please enter a valid 5 COLOR RESULTS\n')
+            continue
+
+        wordsLeftExpected, wordsLeft, result = changeValidWords(word, colorResult, wordsLeft)
         print()
-        wordsLeftExpected, wordsLeft = changeValidWords(word, result, wordsLeft)
-        print()
-        print('Here are some words with the highest amount of information after picking {word}:')
+        if (len(result) == 1):
+            print(f'The word is "{result[0]}"')
+            break
+        print('\nHere are some words with the highest amount of information after picking {word}:')
         for i in range(15):
             print(wordsLeftExpected[i][0], wordsLeftExpected[i][1])
+        print("...")
 
